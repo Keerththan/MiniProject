17b6756143a7633135daf5772f11ae3da9183f34
Keerththan <keerththant@gmail.com>
2023-01-22 21:59:59 +0530

    First Commit J

--- Project1.C
+++ Project1.C
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main() {
    int age;
-   printf("Enter Your age\n");
+   printf("Enter Your ageJ\n");
    scanf("%d",&age);
 
    printf("Your Age is %d",age);
    return 0;
-}10
+}
